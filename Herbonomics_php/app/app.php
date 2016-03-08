bf203e1fb86139b8cd2568865b96e3604f571b09
Adam Ross Russell <student@epicodus.com>
2016-03-08 10:03:04 -0800

    add dispensary_account html template and new findbydispensary function in dispensary demand

--- Herbonomics_php/app/app.php
+++ Herbonomics_php/app/app.php
@@ -31,15 +31,58 @@
         return $app['twig']->render('index.html.twig');
     });
 
+    $app->get("/login", function() use ($app) {
+        return $app['twig']->render('login.html.twig');
+    });
+
+    $app->get("/sign_up", function() use ($app) {
+        return $app['twig']->render('sign_up.html.twig');
+    });
+
     $app->get("/dispensary/sign_in", function() use ($app) {//get or post?
 
         $dispensary = Dispensary::signIn($_GET['username'], $_GET['password']);
 
-        if($dispensary = null) {
+        if ($dispensary == null) {
             return $app['twig']->render('login.html.twig');
         } else
 
         return $app['twig']->render('dispensary_account.html.twig', array('dispensary' => $dispensary));
+    });
+
+    $app->get("/grower/sign_in", function() use ($app) {//get or post?
+
+        $grower = Grower::signIn($_GET['username'], $_GET['password']);
+
+        if ($grower == null) {
+            return $app['twig']->render('login.html.twig');
+        } else
+
+        return $app['twig']->render('grower_account.html.twig', array('grower' => $grower));
+    });
+
+    $app->post("/dispensary/sign_up", function() use ($app) {//get or post?
+        $name = $_POST['name'];
+        $website = $_POST['website'];
+        $email = $_POST['email'];
+        $username = $_POST['username'];
+        $password = $_POST['password'];
+        $dispensary = new Dispensary($name, $website, $email, $username, $password);
+        $dispensary->save();
+
+        return $app['twig']->render('dispensary_account.html.twig', array('dispensary' => $dispensary));
+    });
+
+    $app->post("/grower/sign_up", function() use ($app) {//get or post?
+        $name = $_POST['name'];
+        $website = $_POST['website'];
+        $email = $_POST['email'];
+        $username = $_POST['username'];
+        $password = $_POST['password'];
+        $grower = new Grower($name, $website, $email, $username, $password);
+        $grower->save();
+
+        return $app['twig']->render('grower_account.html.twig', array('grower' => $grower));
     });
 
     return $app;
